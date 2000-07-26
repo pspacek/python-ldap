5a2c24d3d7f42808ef1069ef80f2432078b0aa57
leonard <leonard>
2000-07-26 10:25:03 +0000

    first hack at template support

---
+++ ldapmodule/template.h
@@ -0,0 +1,44 @@
+/* David Leonard <david.leonard@csee.uq.edu.au>, 2000. Public domain. */
+#ifndef __h_TemplateObject 
+#define __h_TemplateObject 
+
+/* $Id: template.h,v 1.1 2000/07/26 10:25:03 leonard Exp $ */
+
+#if defined(HAVE_LDAP_INIT_TEMPLATES)
+
+typedef struct {
+	PyObject_HEAD
+	struct ldap_disptmpl	*tmpllist;
+} DispTmplSeqObject;
+
+typedef struct {
+	PyObject_HEAD
+	DispTmplSeqObject	*tmplseqobj;
+	struct ldap_disptmpl	*disptmpl;
+} DispTmplObject;
+
+typedef struct {
+	PyObject_HEAD
+	DispTmplObject		*tmplobj;
+} DispTmplRowSeqObject;
+
+typedef struct {
+	PyObject_HEAD
+	DispTmplObject		*tmplobj;
+	struct ldap_tmplitem	*row;
+} DispTmplColSeqObject;
+
+typedef struct {
+	PyObject_HEAD
+	DispTmplObject *tmplobj;
+	struct ldap_tmplitem *item;
+} DispTmplItemObject;
+
+void LDAPinit_template(PyObject *);
+PyObject *l_init_templates(PyObject *, PyObject *);
+extern char l_init_templates_doc[];
+
+#endif /* HAVE_LDAP_INIT_TEMPLATES */
+
+#endif /* __h_TemplateObject */
+
