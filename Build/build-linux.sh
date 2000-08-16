b74832254445e0b26bb389b509bb408803b7b9f7
leonard <leonard>
2000-08-16 12:24:39 +0000

    new linux build, using rpm

--- Build/build-linux.sh
+++ Build/build-linux.sh
@@ -2,29 +2,14 @@
 
 # Locate the top of the distribution
 srcdir=`dirname $0`/..
+version=`sh ${srcdir}/Build/version.sh`
+release=1
+specfile=python-ldap-$version-$release.spec
 
-#re-sync
-#(cd $srcdir && cvs up -PAd && autoreconf)
+rm -f ${specfile}
+sed -e "s/@version@/$version/g" \
+    -e "s/@release@/$release/g" \
+    < ${srcdir}/Build/template.spec \
+    > ${specfile}
 
-# clean up a bit
-rm -rf Makefile Modules config.cache config.log config.status
-
-# build openldap as required
-rm -rf /tmp/ldap-pfx
-mkdir -p ldap-pfx
-ln -s `pwd`/ldap-pfx /tmp/ldap-pfx
-sh $srcdir/Misc/openldap.sh
-
-# perform an independent build
-sh $srcdir/configure
-make
-
-# install under a different root
-rm -rf destdir; mkdir destdir
-mkdir -p destdir/usr
-DESTDIR=`pwd`/destdir make install
-
-# show what files were installed
-(cd destdir && find . -type f -print | sort)
-
-echo XXX need to find out how to use rpm -b to build a package
+rpm -b --buildroot `pwd`/root --test $specfile
