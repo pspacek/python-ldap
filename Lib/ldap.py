35b1ada98127be85a658c277ba7e6dc844c49ef8
stroeder <stroeder>
2001-11-13 15:01:09 +0000

    Converted module ldap to directory-base module package.

--- Lib/ldap.py
+++
@@ -1,3 +0,0 @@
-# $Id: ldap.py,v 1.2 2000/08/14 22:37:44 leonard Exp $
-from _ldap import __version__
-from _ldap import *
---
+++ Lib/ldap/__init__.py
@@ -0,0 +1,4 @@
+# $Id: __init__.py,v 1.1 2001/11/13 15:01:09 stroeder Exp $
+from _ldap import __version__
+from _ldap import *
+
