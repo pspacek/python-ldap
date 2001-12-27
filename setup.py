3d3360e7183b1bff96f2752d9a68ae2029d11dab
stroeder <stroeder>
2001-12-27 17:19:29 +0000

    Use keyword parameter packages for include the whole module package ldap

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-# $Id: setup.py,v 1.13 2001/12/24 15:41:24 stroeder Exp $
+# $Id: setup.py,v 1.14 2001/12/27 17:19:29 stroeder Exp $
 
 from distutils.core import setup, Extension
 from ConfigParser import ConfigParser
@@ -82,15 +82,11 @@ setup(
 	],
 	#-- Python modules
 	py_modules = [
-                'ldap',
-                'ldap.async',
-                'ldap.ldapobject',
-                'ldap.modlist',
                 'ldapurl',
 		'ldif',
 		#'perldap',
 	],
-
+        packages = ['ldap'],
 	#-- where to find the python modules
 	package_dir = { '': 'Lib' },
 )
