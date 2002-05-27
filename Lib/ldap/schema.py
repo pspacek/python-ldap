a89ec40bd09def7cb285e65e5c1ed873b4d16795
stroeder <stroeder>
2002-05-27 11:53:10 +0000

    One OID dictionary as registry for all schema elements. Some renaming.

--- Lib/ldap/schema.py
+++ Lib/ldap/schema.py
@@ -2,30 +2,33 @@
 schema.py - support for subSchemaSubEntry information
 written by Hans Aschauer <Hans.Aschauer@Physik.uni-muenchen.de>
 
-\$Id: schema.py,v 1.3 2002/05/04 18:47:23 stroeder Exp $
+\$Id: schema.py,v 1.4 2002/05/27 11:53:10 stroeder Exp $
 
 License:
 Public domain. Do anything you want with this module.
 """
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
-import ldap, _ldap
 
-def str2objectclass(str):
-    return ldap._ldap_call(_ldap.str2objectclass,str)
-def str2attributetype(str):
-    return ldap._ldap_call(_ldap.str2attributetype,str)
-def str2syntax(str):
-    return ldap._ldap_call(_ldap.str2syntax,str)
-def str2matchingrule(str):
-    return ldap._ldap_call(_ldap.str2matchingrule,str)
+import ldap,ldap.functions,_ldap
 
+
+# Wrapper functions to serialize calls into OpenLDAP libs with
+# a module-wide thread lock
+def str2objectclass(schema_element_str):
+    return ldap.functions._ldap_call(_ldap.str2objectclass,schema_element_str)
+def str2attributetype(schema_element_str):
+    return ldap.functions._ldap_call(_ldap.str2attributetype,schema_element_str)
+def str2syntax(schema_element_str):
+    return ldap.functions._ldap_call(_ldap.str2syntax,schema_element_str)
+def str2matchingrule(schema_element_str):
+    return ldap.functions._ldap_call(_ldap.str2matchingrule,schema_element_str)
 
 
 class objectClass:
-    def __init__(self, str):
-        (self.oid,           #REQUIRED 
+    def __init__(self, schema_element_str):
+        (self.oid,           #REQUIRED
          self.names,         #OPTIONAL
          self.desc,          #OPTIONAL
          self.obsolete,      #0=no, 1=yes
@@ -34,109 +37,105 @@ class objectClass:
          self.oids_must,     #OPTIONAL
          self.oids_may,      #OPTIONAL
          self.ext,           #OPTIONAL
-         ) = str2objectclass(str)
+         ) = str2objectclass(schema_element_str)
 
 class attributeType:
-    def __init__(self, str):
-        (self.oid,             #REQUIRED				    
-         self.names,           #OPTIONAL				    
-         self.desc,            #OPTIONAL				    
-         self.obsolete,        #0=no, 1=yes 			    
-         self.sup_oid,         #OPTIONAL				    
-         self.equaltity_oid,   #OPTIONAL				    
-         self.ordering_oid,    #OPTIONAL				    
-         self.substr_oid,      #OPTIONAL				    
-         self.syntax_oid,      #OPTIONAL				    
-         self.syntax_len,      #OPTIONAL				    
+    def __init__(self, schema_element_str):
+        (self.oid,             #REQUIRED
+         self.names,           #OPTIONAL
+         self.desc,            #OPTIONAL
+         self.obsolete,        #0=no, 1=yes
+         self.sup_oid,         #OPTIONAL
+         self.equality_oid,    #OPTIONAL
+         self.ordering_oid,    #OPTIONAL
+         self.substr_oid,      #OPTIONAL
+         self.syntax_oid,      #OPTIONAL
+         self.syntax_len,      #OPTIONAL
          self.single_value,    #0=no, 1=yes		    
          self.collectiove,     #0=no, 1=yes			    
          self.no_user_mod,     #0=no, 1=yes 			    
          self.usage,           #0=userApplications, 1=directoryOperation,
                                #2=distributedOperation, 3=dSAOperation
          self.ext              #OPTIONAL
-         ) = str2attributetype(str)
+         ) = str2attributetype(schema_element_str)
 
 class ldapSyntax:
-    def __init__(self, str):
+    def __init__(self, schema_element_str):
         (self.oid,    #REQUIRED
          self.names,  #OPTIONAL
          self.desc,   #OPTIONAL
          self.ext     #OPTIONAL
-         ) = str2syntax(str)
+         ) = str2syntax(schema_element_str)
 
 class matchingRule:
-    def __init__(self, str):
+    def __init__(self, schema_element_str):
         (self.oid,         #REQUIRED
          self.names,       #OPTIONAL
          self.desc,        #OPTIONAL
          self.obsolete,    #OPTIONAL
          self.syntax_oid,  #REQUIRED
          self.ext          #OPTIONAL
-         ) = str2matchingrule(str)
+         ) = str2matchingrule(schema_element_str)
 
-class rootDSESchema:
-    oc="objectClasses"
-    at="attributeTypes"
-    syn="ldapSyntaxes"
-    mr="matchingRules"
+
+SCHEMA_CLASS_MAPPING = {
+  'objectClasses':objectClass,
+  'attributeTypes':attributeType,
+  'ldapSyntaxes':ldapSyntax,
+  'matchingRules':matchingRule
+}
+SCHEMA_ATTRS = SCHEMA_CLASS_MAPPING.keys()
+
+
+class subSchema:
     
-    def __init__(self, l):
-        self.objectClasses = {}
-        self.objectClassesByName = {}
-        self.attributeTypes = {}
-        self.attributeTypesByName = {}
-        self.ldapSyntaxes = {}
-        self.ldapSyntaxesByName = {}
-        self.matchingRules = {}
-        self.matchingRulesByName = {}
-        type = self.oc
-        result = l.search_s("cn=Subschema",
-                            ldap.SCOPE_BASE,
-                            "(objectClass=*)",
-                            [type])
-        for schema_string in result[0][1][type]:
-            oc = objectClass(schema_string)
-            self.objectClasses[oc.oid] = oc
-            for name in oc.names:
-                self.objectClassesByName[name]= oc
-        
-        type = self.at
-        result = l.search_s("cn=Subschema",
-                            ldap.SCOPE_BASE,
-                            "(objectClass=*)",
-                            [type])
-        for schema_string in result[0][1][type]:
-            at = attributeType(schema_string)
-            self.attributeTypes[at.oid] = at
-            for name in at.names:
-                self.attributeTypesByName[name]= at
-        
-        type = self.syn
-        result = l.search_s("cn=Subschema",
-                            ldap.SCOPE_BASE,
-                            "(objectClass=*)",
-                            [type])
-        for schema_string in result[0][1][type]:
-            syn = ldapSyntax(schema_string)
-            self.ldapSyntaxes[syn.oid] = syn
-            for name in syn.names:
-                self.ldapSyntaxesByName[name]= syn
-        
-        type = self.mr
-        result = l.search_s("cn=Subschema",
-                            ldap.SCOPE_BASE,
-                            "(objectClass=*)",
-                            [type])
-        for schema_string in result[0][1][type]:
-            mr = matchingRule(schema_string)
-            self.matchingRules[mr.oid] = mr
-            for name in mr.names:
-                self.matchingRulesByName[name]= mr
-        
+    def __init__(self,l,schema_dn):
+        """
+        l
+                LDAPObject instance
+        schema_dn
+                Distinguished name of sub schema sub entry to read
+        """
+        self.schema_element = {}
+        self.name2oid = {}
+        result = l.search_s(
+          schema_dn,
+          ldap.SCOPE_BASE,"(objectClass=*)",
+          SCHEMA_ATTRS
+        )
+
+        if not result:
+          # Nothing to do for empty search result
+          return
+
+        # Sub schema sub entry's data
+        sub_schema_sub_entry = result[0][1]
+        # Build the schema registry
+        for attr_type in SCHEMA_ATTRS:
+          if not sub_schema_sub_entry[attr_type]:
+            continue
+          for attr_value in sub_schema_sub_entry[attr_type]:
+            se = SCHEMA_CLASS_MAPPING[attr_type](attr_value)
+            assert not self.schema_element.has_key(se.oid), ValueError
+            self.schema_element[se.oid] = se
+            for name in se.names:
+              self.name2oid[name] = se.oid
+
+        return # subSchema.__init__()        
+
 
 if __name__ == '__main__':
-    l=ldap.initialize("ldap://localhost:1389")
-    l.simple_bind_s("","")
+    l=ldap.initialize('ldap://localhost:1389')
+    l.simple_bind_s('','')
 
-    schema = rootDSESchema(l)
-    
+    schema = subSchema(l,'cn=Subschema')
+    for attr_type,schema_class in SCHEMA_CLASS_MAPPING.items():
+      print '***',repr(attr_type),'***'
+      for oid,se in schema.schema_element.items():
+        if isinstance(se,schema_class):
+          print repr(oid),repr(se)
+    schema_element_names = schema.name2oid.keys()
+    schema_element_names.sort()
+    for name in schema_element_names:
+      print repr(name),'->',repr(schema.name2oid[name])
+
