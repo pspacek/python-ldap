923002f8d6a7d8be9cad77ff5c5e381ed6d073ba
stroeder <stroeder>
2002-08-02 13:36:08 +0000

    Time measuring and use of subSchema.all_attrs()

--- Demo/schema.py
+++ Demo/schema.py
@@ -1,20 +1,34 @@
-import sys,ldap,ldap.schema,ldapurl
+import sys,time,ldap,ldap.schema,ldapurl
+
+schema_allow = ldap.schema.ALLOW_ALL
 
 ldap_url = ldapurl.LDAPUrl(sys.argv[1])
 
 ldap.set_option(ldap.OPT_DEBUG_LEVEL,0)
+
+ldap._trace_level = 0
 
 # Connect and bind as LDAPv3
 l=ldap.initialize(ldap_url.initializeUrl(),trace_level=0)
 l.protocol_version = ldap.VERSION3
 l.simple_bind_s('','')
 
+time_mark0 = time.time()
+
 # Search for DN of sub schema sub entry
 subschemasubentry_dn = l.search_subschemasubentry_s(ldap_url.dn.encode('utf-8'))
+
+time_mark1 = time.time()
+
+print 'Time elapsed search sub schema sub entry: %0.3f' % (time_mark1-time_mark0)
 
 subschemasubentry_entry = l.read_subschemasubentry_s(
   subschemasubentry_dn
 )
+
+time_mark2 = time.time()
+
+print 'Time elapsed reading sub schema sub entry: %0.3f' % (time_mark2-time_mark1)
 
 if subschemasubentry_dn is None:
   print 'No sub schema sub entry found!'
@@ -25,8 +39,12 @@ else:
 
   # Read the schema entry
   schema = ldap.schema.subSchema(
-    subschemasubentry_entry,schema_allow=ldap.schema.ALLOW_ALL
+    subschemasubentry_entry,schema_allow=schema_allow
   )
+
+  time_mark3 = time.time()
+
+  print 'Time elapsed parsing sub schema sub entry: %0.3f' % (time_mark3-time_mark2)
 
   # Display schema
   for attr_type,schema_class in ldap.schema.SCHEMA_CLASS_MAPPING.items():
@@ -41,5 +59,5 @@ else:
   print '*** inetOrgPerson ***'
   inetOrgPerson = schema.schema_element[schema.name2oid['inetOrgPerson']]
   print inetOrgPerson.must,inetOrgPerson.may
-  all_must,all_may = inetOrgPerson.all_attrs(schema)
-  print all_must,all_may
+  print '*** person,organizationalPerson,inetOrgPerson ***'
+  print schema.all_attrs(['person','organizationalPerson','inetOrgPerson'])
