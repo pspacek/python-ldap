ab141d3722ad51b7305dd9335942ea7a4a593b07
stroeder <stroeder>
2002-08-08 07:42:03 +0000

    Exit if subschemasubentry_dn is None

--- Demo/schema.py
+++ Demo/schema.py
@@ -22,49 +22,47 @@ subschemasubentry_dn = l.search_subschemasubentry_s(ldap_url.dn.encode('utf-8'))
 
 time_mark1 = time.time()
 
+print 'Result of search for sub schema sub entry:',repr(subschemasubentry_dn)
 print 'Time elapsed search sub schema sub entry: %0.3f' % (time_mark1-time_mark0)
 
-# Read the sub schema sub entry
-
-subschemasubentry_entry = l.read_subschemasubentry_s(
-  subschemasubentry_dn,attrs=schema_attrs
-)
-
-time_mark2 = time.time()
-
-print 'Time elapsed reading sub schema sub entry: %0.3f' % (time_mark2-time_mark1)
 
 if subschemasubentry_dn is None:
   print 'No sub schema sub entry found!'
+  sys.exit(1)
 
-else:
+# Read the sub schema sub entry
+subschemasubentry_entry = l.read_subschemasubentry_s(
+  subschemasubentry_dn,attrs=schema_attrs
+)
+time_mark2 = time.time()
+print 'Time elapsed reading sub schema sub entry: %0.3f' % (time_mark2-time_mark1)
 
-  print '*** Schema from',repr(subschemasubentry_dn)
+print '*** Schema from',repr(subschemasubentry_dn)
 
-  # Parse the schema entry
-  schema = ldap.schema.subSchema(
-    subschemasubentry_entry,schema_allow=schema_allow
-  )
+# Parse the schema entry
+schema = ldap.schema.subSchema(
+  subschemasubentry_entry,schema_allow=schema_allow
+)
 
-  time_mark3 = time.time()
+time_mark3 = time.time()
 
-  print 'Time elapsed parsing sub schema sub entry: %0.3f' % (time_mark3-time_mark2)
+print 'Time elapsed parsing sub schema sub entry: %0.3f' % (time_mark3-time_mark2)
 
-  schema_element_names = schema.name2oid.keys()
-  schema_element_names.sort()
-  for name in schema_element_names:
-    print repr(name),'->',repr(schema.name2oid[name])
+schema_element_names = schema.name2oid.keys()
+schema_element_names.sort()
+for name in schema_element_names:
+  print repr(name),'->',repr(schema.name2oid[name])
 
-  # Display schema
-  for attr_type,schema_class in ldap.schema.SCHEMA_CLASS_MAPPING.items():
-    print '***',repr(attr_type),'***'
-    for oid,se in schema.schema_element.items():
-      if isinstance(se,schema_class):
-        print repr(oid),repr(se)
-  print '*** Testing object class inetOrgPerson ***'
-  inetOrgPerson = schema.schema_element[schema.name2oid['inetOrgPerson']]
-  print inetOrgPerson.must,inetOrgPerson.may
-  print '*** person,organizationalPerson,inetOrgPerson ***'
-  print schema.all_attrs(
-    ['person','organizationalPerson','inetOrgPerson']
-  )
+# Display schema
+for attr_type,schema_class in ldap.schema.SCHEMA_CLASS_MAPPING.items():
+  print '***',repr(attr_type),'***'
+  for oid,se in schema.schema_element.items():
+    if isinstance(se,schema_class):
+      print repr(oid),repr(se)
+print '*** Testing object class inetOrgPerson ***'
+inetOrgPerson = schema.schema_element[schema.name2oid['inetOrgPerson']]
+print inetOrgPerson.must,inetOrgPerson.may
+print '*** person,organizationalPerson,inetOrgPerson ***'
+print schema.all_attrs(
+  ['person','organizationalPerson','inetOrgPerson']
+)
