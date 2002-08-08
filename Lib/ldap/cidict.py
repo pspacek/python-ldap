d679451f6fea86be3625ae9951c6c8239b5b362a
stroeder <stroeder>
2002-08-08 13:41:55 +0000

    Added following methods to cidict: .get(), .keys() and .items(). Case-respective handling of keys.

--- Lib/ldap/cidict.py
+++ Lib/ldap/cidict.py
@@ -1,30 +1,58 @@
-# $Id: cidict.py,v 1.3 2002/08/08 07:25:38 stroeder Exp $
 """
-	This is a convenience wrapper for dictionaries
-	returned from LDAP servers containing attribute
-	names of variable case.
+This is a convenience wrapper for dictionaries
+returned from LDAP servers containing attribute
+names of variable case.
+
+$Id: cidict.py,v 1.4 2002/08/08 13:41:55 stroeder Exp $
 """
 
-__version__ = """$Revision: 1.3 $"""
+__version__ = """$Revision: 1.4 $"""
 
 from UserDict import UserDict
 from string import lower
 
 class cidict(UserDict):
-	"""Case-insensitive dictionary."""
-	def __init__(self, default = {}):
-		d = {}
-		for k,v in default.items():
-			d[lower(k)] = v;
-		UserDict.__init__(self, d)
-	def __getitem__(self, name):
-		return self.data[lower(name)]
-	def __setitem__(self, name, value):
-		self.data[lower(name)] = value
-	def __delitem__(self, name):
-		del self.data[lower(name)]
-	def has_key(self, name):
-		return UserDict.has_key(self, lower(name))
+  """
+  Case-insensitive bud case-respecting dictionary.
+  """
+
+  def __init__(self,default = {}):
+    self._keys = {}
+    d = {}
+    for k,v in default.items():
+      lower_k = lower(k)
+      self._keys[lower_k] = k
+      d[lower_k] = v
+    UserDict.__init__(self,d)
+
+  def __getitem__(self,key):
+    return self.data[lower(key)]
+
+  def __setitem__(self,key,value):
+    lower_key = lower(key)
+    self._keys[lower_key] = key
+    self.data[lower_key] = value
+
+  def __delitem__(self,key):
+    del self.data[lower(key)]
+
+  def has_key(self,key):
+    return UserDict.has_key(self,lower(key))
+
+  def get(self,key,failobj):
+    try:
+      return self[key]
+    except KeyError:
+      return failobj
+
+  def keys(self):
+    return self._keys.values()
+
+  def items(self):
+    result = []
+    for k in self._keys.values():
+      result.append((k,self[k]))
+    return result
 
 
 def strlist_minus(a,b):
@@ -70,15 +98,20 @@ def strlist_union(a,b):
   return temp.values()
 
 
-if __name__ == '__main__':
-	x = { 'AbCDeF' : 123 }
-	cix = cidict(x)
-	assert cix["ABCDEF"] == 123
-	assert cix.get("ABCDEF",None) == 123
-	assert cix.get("not existent",None) is None
-	cix["xYZ"] = 987
-	assert cix["XyZ"] == 987
-	assert cix.get("XyZ",None) == 987
-	del cix["abcdEF"]
-	assert not cix.has_key("AbCDef")
-
+if __debug__ and __name__ == '__main__':
+  x = { 'AbCDeF' : 123 }
+  cix = cidict(x)
+  assert cix["ABCDEF"] == 123
+  assert cix.get("ABCDEF",None) == 123
+  assert cix.get("not existent",None) is None
+  cix["xYZ"] = 987
+  assert cix["XyZ"] == 987
+  assert cix.get("XyZ",None) == 987
+  cix_keys = cix.keys()
+  cix_keys.sort()
+  assert cix_keys==['AbCDeF','xYZ'],ValueError(repr(cix_keys))
+  cix_items = cix.items()
+  cix_items.sort()
+  assert cix_items==[('AbCDeF',123),('xYZ',987)],ValueError(repr(cix_items))
+  del cix["abcdEF"]
+  assert not cix.has_key("AbCDef")
