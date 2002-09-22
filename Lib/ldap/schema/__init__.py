8e9093d67311303d96e5b305aab744a8bce83b89
stroeder <stroeder>
2002-09-22 02:10:39 +0000

    Also import SCHEMA_ATTR_MAPPING

--- Lib/ldap/schema/__init__.py
+++ Lib/ldap/schema/__init__.py
@@ -4,10 +4,10 @@ written by Michael Stroeder <michael@stroeder.com>
 
 See http://python-ldap.sourceforge.net for details.
 
-\$Id: __init__.py,v 1.2 2002/09/06 07:15:01 stroeder Exp $
+\$Id: __init__.py,v 1.3 2002/09/22 02:10:39 stroeder Exp $
 """
 
 __version__ = '0.2.0'
 
-from ldap.schema.subentry import SubSchema,SCHEMA_ATTRS,SCHEMA_CLASS_MAPPING,urlfetch
+from ldap.schema.subentry import SubSchema,SCHEMA_ATTRS,SCHEMA_CLASS_MAPPING,SCHEMA_ATTR_MAPPING,urlfetch
 from ldap.schema.models import *
