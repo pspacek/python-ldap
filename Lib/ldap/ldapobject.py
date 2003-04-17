40906170f479210be3250a1e7c6d5bb46f43d96d
stroeder <stroeder>
2003-04-17 14:16:09 +0000

    apply() is not used anymore since it seems deprecated

--- Lib/ldap/ldapobject.py
+++ Lib/ldap/ldapobject.py
@@ -4,7 +4,7 @@ written by Michael Stroeder <michael@stroeder.com>
 
 See http://python-ldap.sourceforge.net for details.
 
-\$Id: ldapobject.py,v 1.53 2003/03/25 17:21:15 stroeder Exp $
+\$Id: ldapobject.py,v 1.54 2003/04/17 14:16:10 stroeder Exp $
 
 Compability:
 - Tested with Python 2.0+ but should work with Python 1.5.x
@@ -84,7 +84,7 @@ class SimpleLDAPObject:
     self._ldap_object_lock.acquire()
     try:
       try:
-        result = apply(func,args,kwargs)
+        result = func(*args,**kwargs)
       finally:
         self._ldap_object_lock.release()
     except LDAPError,e:
@@ -635,7 +635,7 @@ class ReconnectLDAPObject(SimpleLDAPObject):
   def _apply_last_bind(self):
     if self._last_bind!=None:
       func,args,kwargs = self._last_bind
-      apply(func,args,kwargs)
+      func(*args,**kwargs)
 
   def _restore_options(self):
     """Restore all recorded options"""
@@ -681,12 +681,12 @@ class ReconnectLDAPObject(SimpleLDAPObject):
 
   def _apply_method_s(self,func,*args,**kwargs):
     try:
-      return apply(func,args,kwargs)
+      return func(*args,**kwargs)
     except ldap.SERVER_DOWN:
       # Reconnect
       self.reconnect(self._uri)
       # Re-try last operation
-      return apply(func,args,kwargs)
+      return func(*args,**kwargs)
 
   def set_option(self,option,invalue):
     self._options[option] = invalue
