69aff8a03b6ca20b7f22d016afb38e5643500247
stroeder <stroeder>
2003-05-02 06:52:09 +0000

    Cosmetic change: Named argument list for LDAPObject.compare() instead of *args,**kwargs.

--- Lib/ldap/ldapobject.py
+++ Lib/ldap/ldapobject.py
@@ -4,7 +4,7 @@ written by Michael Stroeder <michael@stroeder.com>
 
 See http://python-ldap.sourceforge.net for details.
 
-\$Id: ldapobject.py,v 1.55 2003/04/19 02:10:25 stroeder Exp $
+\$Id: ldapobject.py,v 1.56 2003/05/02 06:52:09 stroeder Exp $
 
 Compability:
 - Tested with Python 2.0+ but should work with Python 1.5.x
@@ -19,7 +19,7 @@ Basically calls into the LDAP lib are serialized by the module-wide
 lock self._ldap_object_lock.
 """
 
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 
 __all__ = [
   'LDAPObject',
@@ -159,7 +159,7 @@ class SimpleLDAPObject:
     """
     return self._ldap_call(self._l.sasl_bind_s,who,auth)
 
-  def compare(self,*args,**kwargs):
+  def compare(self,dn,attr,value):
     """
     compare(dn, attr, value) -> int
     compare_s(dn, attr, value) -> int    
@@ -175,10 +175,10 @@ class SimpleLDAPObject:
         A design bug in the library prevents value from containing
         nul characters.
     """
-    return self._ldap_call(self._l.compare,*args,**kwargs)
+    return self._ldap_call(self._l.compare,dn,attr,value)
 
-  def compare_s(self,*args,**kwargs):
-    msgid = self.compare(*args,**kwargs)
+  def compare_s(self,dn,attr,value):
+    msgid = self.compare(dn,attr,value)
     try:
       self.result(msgid,all=1,timeout=self.timeout)
     except _ldap.COMPARE_TRUE:
