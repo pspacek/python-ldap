4cf6e99b41900082f6a1ae28636ebc1aae20df4b
stroeder <stroeder>
2003-05-22 12:17:25 +0000

    escape_filter_chars(): back-slash must be quoted at first, use raw strings where possible

--- Lib/ldap/filter.py
+++ Lib/ldap/filter.py
@@ -4,7 +4,7 @@ written by Michael Stroeder <michael@stroeder.com>
 
 See http://python-ldap.sourceforge.net for details.
 
-\$Id: filter.py,v 1.1 2003/05/11 13:33:03 stroeder Exp $
+\$Id: filter.py,v 1.2 2003/05/22 12:17:25 stroeder Exp $
 
 Compability:
 - Tested with Python 2.0+
@@ -18,10 +18,10 @@ def escape_filter_chars(assertion_value):
   Replace all special characters found in assertion_value
   by quoted notation  
   """
-  s = assertion_value.replace('*', '\\2a')
-  s = s.replace('(', '\\28')
-  s = s.replace(')', '\\29')
-  s = s.replace('\\', '\\5c')
-  s = s.replace('\x00', '\\00')
+  s = assertion_value.replace('\\', r'\5c')
+  s = s.replace(r'*', r'\2a')
+  s = s.replace(r'(', r'\28')
+  s = s.replace(r')', r'\29')
+  s = s.replace('\x00', r'\00')
   return s 
 
