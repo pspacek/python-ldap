d551d31965d6d2e9506e390f08c40bba2ffc6a89
stroeder <stroeder>
2003-05-26 08:34:03 +0000

    Added function filter_format().

--- Lib/ldap/filter.py
+++ Lib/ldap/filter.py
@@ -4,13 +4,13 @@ written by Michael Stroeder <michael@stroeder.com>
 
 See http://python-ldap.sourceforge.net for details.
 
-\$Id: filter.py,v 1.2 2003/05/22 12:17:25 stroeder Exp $
+\$Id: filter.py,v 1.3 2003/05/26 08:34:03 stroeder Exp $
 
 Compability:
 - Tested with Python 2.0+
 """
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 
 def escape_filter_chars(assertion_value):
@@ -25,3 +25,13 @@ def escape_filter_chars(assertion_value):
   s = s.replace('\x00', r'\00')
   return s 
 
+
+def filter_format(filter_template,assertion_values):
+  """
+  filter_template
+        String containing %s as placeholder for assertion values.
+  assertion_values
+        List or tuple of assertion values. Length must match
+        count of %s in filter_template.
+  """
+  return filter_template % (tuple(map(escape_filter_chars,assertion_values)))
