e2b333fc52534b746f1d7d1c414d96721e43cf21
stroeder <stroeder>
2004-03-18 19:30:40 +0000

    Use only ldap.sasl.sasl, demonstrate whoami_s()

--- Demo/sasl_bind.py
+++ Demo/sasl_bind.py
@@ -1,14 +1,59 @@
 # For documentation, see comments in Module/LDAPObject.c and the
 # ldap.sasl module documentation.
 
-import ldap, ldap.sasl
+import ldap,ldap.sasl
 
-l = ldap.initialize("ldap://localhost:1390/")
-auth = ldap.sasl.digest_md5("michael","secret")
-l.sasl_bind_s("", auth)
-    
+ldap.sasl._trace_level=1
 
-res = l.search_s("",ldap.SCOPE_BASE,"(objectClass=*)")
-print res
+ldap.set_option(ldap.OPT_DEBUG_LEVEL,9)
 
-l.unbind()
+for ldap_uri,sasl_mech,sasl_cb_value_dict in [
+  (
+    "ldap://localhost:1390/",
+    'CRAM-MD5',
+    {
+      ldap.sasl.CB_AUTHNAME    :'fred',
+      ldap.sasl.CB_PASS        :'secret',
+    }
+  ),
+  (
+    "ldap://localhost:1390/",
+    'PLAIN',
+    {
+      ldap.sasl.CB_AUTHNAME    :'fred',
+      ldap.sasl.CB_PASS        :'secret',
+    }
+  ),
+  (
+    "ldap://localhost:1390/",
+    'LOGIN',
+    {
+      ldap.sasl.CB_AUTHNAME    :'fred',
+      ldap.sasl.CB_PASS        :'secret',
+    }
+  ),
+  (
+    "ldapi://%2Ftmp%2Fopenldap-socket/",
+    'EXTERNAL',
+    { }
+  ),
+  (
+    "ldap://localhost:1390/",
+    'DIGEST-MD5',
+    {
+      ldap.sasl.CB_AUTHNAME    :'fred',
+      ldap.sasl.CB_PASS        :'secret',
+    }
+  ),
+]:
+  sasl_auth = ldap.sasl.sasl(sasl_cb_value_dict,sasl_mech)
+  print 20*'*',sasl_auth.mech,20*'*'
+  l = ldap.initialize(ldap_uri,trace_level=0)
+  l.protocol_version = 3
+  try:
+    l.sasl_bind_s("", sasl_auth)
+  except ldap.LDAPError,e:
+    print 'Error using SASL mechanism',sasl_auth.mech,str(e)
+  else:
+    print 'Sucessfully bound using SASL mechanism',sasl_auth.mech,'as',repr(l.whoami_s())
+  l.unbind()
