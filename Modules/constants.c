3734ef8a9f6901e41e5e4c83d813dc7b965100d1
stroeder <stroeder>
2010-08-11 18:47:44 +0000

    Correct #ifdef-statement for LDAP_OPT_X_TLS_CRLFILE fixes build with older OpenLDAP libs

--- Modules/constants.c
+++ Modules/constants.c
@@ -1,6 +1,6 @@
 /* constants defined for LDAP
  * See http://www.python-ldap.org/ for details.
- * $Id: constants.c,v 1.47 2010/05/07 13:22:40 stroeder Exp $ */
+ * $Id: constants.c,v 1.48 2010/08/11 18:47:44 stroeder Exp $ */
 
 #include "common.h"
 #include "constants.h"
@@ -181,7 +181,9 @@ LDAPinit_constants( PyObject* d )
 #ifdef LDAP_OPT_X_TLS_CRLCHECK
   /* only available if OpenSSL supports it => might cause backward compability problems */
 	add_int(d,OPT_X_TLS_CRLCHECK);
+#ifdef LDAP_OPT_X_TLS_CRLFILE
 	add_int(d,OPT_X_TLS_CRLFILE);
+#endif
 	add_int(d,OPT_X_TLS_CRL_NONE);
 	add_int(d,OPT_X_TLS_CRL_PEER);
 	add_int(d,OPT_X_TLS_CRL_ALL);
