94d8bc4ff86f464c98b4444858fa10ec222aeb1d
stroeder <stroeder>
2011-04-08 11:31:17 +0000

    Use module ldap.controls.readentry instead of defining LDAP control classes

--- Demo/pyasn1/readentrycontrol.py
+++ Demo/pyasn1/readentrycontrol.py
@@ -9,69 +9,30 @@ Requires module pyasn1 (see http://pyasn1.sourceforge.net/)
 
 import ldap,ldap.sasl
 
-from pyasn1.codec.ber import encoder,decoder
-from ldap.controls import LDAPControl
+from ldap.controls.readentry import PreReadControl,PostReadControl
 
-from pyasn1_modules.rfc2251 import AttributeDescriptionList,SearchResultEntry
-
-
-class ReadEntryControl(LDAPControl):
-  """
-  Base class for control described in RFC 4527
-  """
-
-  def __init__(self,criticality=False,attrList=None):
-    self.criticality,self.attrList,self.entry = criticality,attrList or [],None
-
-  def encodeControlValue(self):
-    attributeSelection = AttributeDescriptionList()
-    for i in range(len(self.attrList)):
-      attributeSelection.setComponentByPosition(i,self.attrList[i])
-    return encoder.encode(attributeSelection)
-
-  def decodeControlValue(self,encodedControlValue):
-    decodedEntry,_ = decoder.decode(encodedControlValue,asn1Spec=SearchResultEntry())
-    decodedEntry.prettyPrint()
-    self.entry = dict([])
-
-
-class PreReadControl(ReadEntryControl):
-  controlType = ldap.CONTROL_PRE_READ
-
-class PostReadControl(ReadEntryControl):
-  controlType = ldap.CONTROL_POST_READ
-
-uri = "ldapi://%2Ftmp%2Fopenldap-socket"
+uri = "ldap://localhost:2389/"
 
 l = ldap.initialize(uri,trace_level=2)
-l.sasl_interactive_bind_s(
-  "",
-  ldap.sasl.sasl({},'EXTERNAL')
-)
+l.simple_bind_s('cn=Directory Manager','Geheimer1234')
 
 pr = PreReadControl(criticality=True,attrList=['uidNumber','gidNumber'])
 
 msg_id = l.modify_ext(
-  "cn=Samba Unix UID Pool,ou=Testing,dc=stroeder,dc=de",
+  "cn=uidcounter,ou=Testing,dc=example,dc=com",
   [(ldap.MOD_INCREMENT, "uidNumber", "1"),(ldap.MOD_INCREMENT, "gidNumber", "1")],
   serverctrls = [pr]
 )
-res = l.result3(
-  msg_id,
-  resp_ctrl_classes={PreReadControl.controlType:PreReadControl}
-)
-print "res:", res
+_,_,_,resp_ctrls = l.result3(msg_id)
+print "resp_ctrls[0].dn:",resp_ctrls[0].dn
+print "resp_ctrls[0].entry:",resp_ctrls[0].entry
 
 pr = PostReadControl(criticality=True,attrList=['cn'])
 msg_id = l.rename(
-  "cn=Samba Unix UID Pool,ou=Testing,dc=stroeder,dc=de",
-  "cn=Samba Unix UID Pool 2",
+  "cn=uidcounter,ou=Testing,dc=example,dc=com",
+  "cn=uidcounter2",
   serverctrls = [pr]
 )
-res = l.result3(
-  msg_id,
-  resp_ctrl_classes={PostReadControl.controlType:PostReadControl}
-)
-print "res:", res
-
-
+_,_,_,resp_ctrls = l.result3(msg_id)
+print "resp_ctrls[0].dn:",resp_ctrls[0].dn
+print "resp_ctrls[0].entry:",resp_ctrls[0].entry
