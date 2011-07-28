26375daca387fbed6971fad1ea6fb50c141759b1
stroeder <stroeder>
2011-07-28 08:22:15 +0000

    doc strings

--- Lib/ldap/resiter.py
+++ Lib/ldap/resiter.py
@@ -3,7 +3,7 @@ ldap.resiter - processing LDAP results with iterators
 
 See http://www.python-ldap.org/ for details.
 
-\$Id: resiter.py,v 1.4 2009/07/26 11:09:58 stroeder Exp $
+\$Id: resiter.py,v 1.5 2011/07/28 08:22:15 stroeder Exp $
 
 Python compability note:
 Requires Python 2.3+
@@ -16,13 +16,13 @@ from ldap import __version__
 
 class ResultProcessor:
   """
-  Mix-in class for ldap.ldapopbject.LDAPObject which adds method allresults().
+  Mix-in class used with ldap.ldapopbject.LDAPObject or derived classes.
   """
 
   def allresults(self,msgid,timeout=-1):
     """
     Generator function which returns an iterator for processing all LDAP operation
-    results of the given msgid.
+    results of the given msgid retrieved with LDAPObject.result3() -> 4-tuple
     """
     result_type,result_list,result_msgid,result_serverctrls = self.result3(msgid,0,timeout)
     while result_type and result_list:
