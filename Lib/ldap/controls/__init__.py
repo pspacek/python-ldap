9ee4802ec89365edde35d1c04d5cf7b9f9069c8c
stroeder <stroeder>
2012-07-10 20:23:44 +0000

    ldap.controls.DecodeControlTuples() now simply ignores PyAsn1Error exception raised during decoding malformed response control values in case of non-critical controls.

--- Lib/ldap/controls/__init__.py
+++ Lib/ldap/controls/__init__.py
@@ -4,7 +4,7 @@ controls.py - support classes for LDAP controls
 
 See http://www.python-ldap.org/ for details.
 
-$Id: __init__.py,v 1.7 2012/04/11 13:31:01 stroeder Exp $
+$Id: __init__.py,v 1.8 2012/07/10 20:23:44 stroeder Exp $
 
 Description:
 The ldap.controls module provides LDAPControl classes.
@@ -35,6 +35,7 @@ __all__ = [
 KNOWN_RESPONSE_CONTROLS = {}
 
 import _ldap,ldap
+from pyasn1.error import PyAsn1Error
 
 
 class RequestControl:
@@ -138,8 +139,13 @@ def DecodeControlTuples(ldapControlTuples,knownLDAPControls=None):
         raise ldap.UNAVAILABLE_CRITICAL_EXTENSION('Received unexpected critical response control with controlType %s' % (repr(controlType)))
     else:
       control.controlType,control.criticality = controlType,criticality
-      control.decodeControlValue(encodedControlValue)
-      result.append(control)
+      try:
+        control.decodeControlValue(encodedControlValue)
+      except PyAsn1Error,e:
+        if criticality:
+          raise e
+      else:
+        result.append(control)
   return result
 
 
